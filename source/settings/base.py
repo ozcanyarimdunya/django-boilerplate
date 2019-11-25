2169b0f64e2ee7dcf235b42d562e864f709e70a3
Özcan Yarımdunya <ozcanyd@gmail.com>
2019-11-25 21:58:51 +0300

    Update base.py

--- source/settings/base.py
+++ source/settings/base.py
@@ -83,7 +83,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets', 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets', 'static-dev', 'local')
+    os.path.join(BASE_DIR, 'assets', 'static-dev')
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
