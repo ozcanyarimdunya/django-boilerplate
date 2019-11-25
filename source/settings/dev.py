3b225f4a1f4e396f862512d497f85a505fcf5b10
Özcan Yarımdunya <ozcanyd@gmail.com>
2019-11-25 21:59:58 +0300

    Update dev.py

--- source/settings/dev.py
+++ source/settings/dev.py
@@ -6,6 +6,27 @@ DATABASES = {
         'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
     }
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'source': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 INSTALLED_APPS += [
     'constance.backends.database',
